548765deb135ad4d16a5368620426bec6145e176
royjonesjr64-creator <royjonesjr64@gmail.com>
2026-05-24 12:12:34 +0700

    fix useNavigate

--- src/pages/Setup.jsx
+++ src/pages/Setup.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 
